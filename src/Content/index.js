cf31f0369fe1b4f9688aa07be11f1025a984306f
Pallavi <dasaripallavi1710@gmail.com>
2024-04-23 23:48:24 +0530

    assignment

---
+++ src/Content/index.js
@@ -0,0 +1,11 @@
+import "./index.css"
+const Content=()=>{
+    return(
+        <div class="container-card">
+            
+                Header Content Goes Here
+      
+        </div> 
+    )
+}
+export default Content
